16355c3f4d54d600eab7568883d63da373cd68a5
Mustafa88A <mustafa0780500@gmail.com>
2024-11-28 16:04:10 +0300

    add Hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,52 @@
 import { HashLink as Link } from "react-router-hash-link";
+import { useState } from "react";
+
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="flex justify-center bg-primary">
-      <div className="bg-[#f4cf48] dark:text-black flex justify-between w-[89%]  ">
+      <div className="bg-[#f4cf48] dark:text-black flex justify-between w-[89%]">
         <img
           className="imge-nav"
           src="https://cdn-icons-png.flaticon.com/128/2412/2412869.png"
           alt=""
         />
 
-        <nav className="text-xs w-[50%] md:w-[30%] md:text-base  ">
-          <ul className="navbar-links">
+        {/* Hamburger Icon */}
+        <button className="block md:hidden text-black" onClick={toggleMenu}>
+          ☰
+        </button>
+
+        {/* Navigation Links */}
+        <nav
+          className={` md:text-base md:w-[30%] w-full h-52 md:h-20  absolute md:static top-20 left-0 bg-[#f4cf48] z-10 ${
+            menuOpen ? "block" : "hidden"
+          } md:block`}
+        >
+          <ul className="navbar-links flex flex-col gap-7  text-[18px] md:flex-row">
             <li>
-              <Link to="#About" smooth>
-                01. About{" "}
+              <Link to="#About" smooth onClick={() => setMenuOpen(false)}>
+                01. About
               </Link>
             </li>
-
             <li>
-              <Link to="#Skills" smooth>
-                02. Skills{" "}
+              <Link to="#Skills" smooth onClick={() => setMenuOpen(false)}>
+                02. Skills
               </Link>
             </li>
-
             <li>
-              <Link to="#Cards" smooth>
-                03. My work{" "}
+              <Link to="#Cards" smooth onClick={() => setMenuOpen(false)}>
+                03. My work
               </Link>
             </li>
-
             <li>
-              <Link to="#Contact" smooth>
-                04. Contact{" "}
+              <Link to="#Contact" smooth onClick={() => setMenuOpen(false)}>
+                04. Contact
               </Link>
             </li>
           </ul>
@@ -40,4 +55,5 @@ function Navbar() {
     </div>
   );
 }
+
 export default Navbar;
